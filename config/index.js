1bfa57006ab8c5935d09c2734cf0512afe0d5083
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2017-04-30 20:17:55 +1000

    update config for production env

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
 const nav = require('./navigationRoutes')
 
 module.exports = function config(env = process.env.NODE_ENV) {
-  const mongoUrl = env && env === 'devlocal' ? '127.0.0.1:27017' : '192.168.99.1:3001'
+  if (env === 'production') mongoUrl = '172.104.33.22:3001'
+  else mongoUrl = env === 'devlocal' ? '127.0.0.1:27017' : '192.168.99.1:3001'
+
   return {
     port: process.env.PORT || 3000,
     mongoUrl: `mongodb://${mongoUrl}/libraryApp`,
